f6b9c344e2f4edb07999f43a86f06de218c6e5b6
Bruno Araújo <brunosousa357@gmail.com>
2021-12-02 23:25:41 -0300

    Subindo arquivos

---
+++ src/main/java/br/com/alura/gerenciador/servlet/HelloWorldServlet.java
@@ -0,0 +1,26 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns ="/helloworld")
+public class HelloWorldServlet extends HttpServlet {
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException{
+		
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h1>Meu primeiro servlet</h1>");
+		out.println("</body>");
+		out.println("</html>");
+		
+	}
+	
+}
